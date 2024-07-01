24953e33cf152f511c9ac780b18ab43e49f3b81c
RItikaSRivastav <srivastavritika86@gmail.com>
2024-07-01 15:52:50 +0530

    Add file

---
+++ app/src/main/java/com/example/audioboxapp/KeyConstants.kt
@@ -0,0 +1,8 @@
+package com.example.audioboxapp
+
+object KeyConstants {
+
+    val appId : Long = 1928829208
+    val appSign : String = "d1b1efcd2928016274b14274f99442c0a9bc07c138062efc242a12dce64047df"
+
+}
